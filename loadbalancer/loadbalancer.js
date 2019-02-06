d805dcda186d117f1a7c23c545cfda9b47eb2906
kaskichandrakant <chandk@thoughtworks.com>
2019-02-06 15:57:38 +0530

    added load balancer accepts post requests

--- loadbalancer/loadbalancer.js
+++ loadbalancer/loadbalancer.js
@@ -5,11 +5,17 @@ const PORT=9000;
 app.use(express.static('public'));
 app.use(express.urlencoded());
 const proxies = ['mera_app','mera_app_2','mera_app_3']
+
 app.get('*',(req,res)=>{
     request(`http://${proxies[0]}:8000${req.url}`,(err,response,body)=>{
         res.send(body)
     })
 })
-
+app.post('*',(req,res)=>{
+    console.log(req.body);
+    request.post(`http://${proxies[0]}:8000${req.url}`,{form:req.body},(err,response,body)=>{
+        res.end()
+    })
+})
 console.log(`listening at Port ${PORT}`);
 app.listen(PORT);

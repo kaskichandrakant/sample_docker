2349747d4aa9e482897321ef4f4311b516271b1f
kaskichandrakant <chandk@thoughtworks.com>
2019-02-07 14:25:29 +0530

    added loadbalancer basic server

--- loadbalancer/loadbalancer.js
+++ loadbalancer/loadbalancer.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const request = require('request');
+const sync_request = require('sync-request');
+
 const app=express();
 const PORT=9000;
 app.use(express.static('public'));
 app.use(express.urlencoded());
 const proxies = ['mera_app','mera_app_2','mera_app_3']
+let free_server;
+setInterval(()=>{
+    proxies.forEach((proxy)=>{
+        setTimeout(()=>{
+            try {
+            let res = request(`http://${proxy}:8000/health`,(err,res,body)=>{
+                if(res.statusCode == 200){
+                    console.log(`Healthy server ${proxy}`);
+                    free_server = proxy;
+                }
+                if(err){
+                    console.log('--------->>>',err);
+                }
+            });
+            } catch (error) {
+                console.log(`Unhealthy server ${proxy} by error \n ${error}`);
+            }
+            
+            return;
+        },5)
+    })
+},2000)
+
 
 app.get('*',(req,res)=>{
-    request(`http://${proxies[0]}:8000${req.url}`,(err,response,body)=>{
+    console.log(`-------------> running on ${free_server}`);
+    console.log(req.url);
+    request(`http://${free_server}:8000${req.url}`,(err,response,body)=>{
         res.send(body)
     })
 })
--- mera_app/server.js
+++ mera_app/server.js
@@ -1,19 +1,29 @@
 const express = require('express');
-const app=express();
-const {add_user,getUser} = require('./dbHandler.js');
-const PORT=8000;
+const app = express();
+const {
+    add_user,
+    getUser
+} = require('./dbHandler.js');
+const PORT = 8000;
 app.use(express.static('public'));
 app.use(express.urlencoded());
 app.listen(PORT);
 
 
-app.post('/add_user',(req,res)=>{
+app.post('/add_user', (req, res) => {
     add_user(req.body.username);
     res.send(req.body);
 })
-app.get('/users',(req,res)=>{
-    getUser(req,res);
+app.get('/users', (req, res) => {
+    getUser(req, res);
 })
-
+app.get('/health', (req, res) => {
+    res.sendStatus(200);
+})
+app.get('/slow', (req, res) => {
+    res.setTimeout(15000,()=>{
+        res.send('working now')
+    })
+})
 
 console.log(`listening at Port ${PORT}`);
